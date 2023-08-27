91414d522332f0b5a8f1ab4200bb881ebc310100
gabrielgws.dev <gabrielgws.dev@gmail.com>
2023-08-27 13:25:35 -0300

    first commit 2.0

---
+++ src/app/about/components/Trips.tsx
@@ -0,0 +1,22 @@
+import { prisma } from '@/lib/prisma';
+import React from 'react';
+
+const getTrips = async () => {
+  const trips  = await prisma.trip.findMany({});
+
+  return trips;
+};
+
+const Trips = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts").then((res) => res.json());
+
+  return (
+    <div>
+      {data.map((i: any) => (
+      <p key={i.id} >{i.title}</p>
+      ))}
+    </div>
+  )
+}
+
+export default Trips
